9b2fd91086de618841a57b1136a28c8930cfab34
BilkisAkterDalia <bilkisadalia@gmail.com>
2024-01-24 22:51:55 +0600

    chnages done

--- Food_Shop/views.py
+++ Food_Shop/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from menu.models import FoodMenu
+from menu.models import FoodMenu,Review
 from category.models import Category
 
 def Home(request, category_slug=None):
     data = FoodMenu.objects.all()
+    reviews = Review.objects.all()
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         data = FoodMenu.objects.filter(category=category)
     category = Category.objects.all()
-    return render(request, 'home.html', {'data': data, 'category': category})
+    return render(request, 'home.html', {'data': data, 'category': category,'reviews': reviews})
